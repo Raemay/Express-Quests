108e5ad64f35b174993a43c35175523b0da89507
Raemay <[melaniebelliard@outlook.fr](mailto:melaniebelliard@outlook.fr)>
2023-05-23 16:59:15 +0200

    Express 02 quest

---
+++ userHandlers.js
@@ -0,0 +1,33 @@
+  const database = require("./database");
+  
+  const getUsers = (req, res) => {
+    database
+      .query("SELECT * FROM users")
+      .then(([rows]) => {
+        res.status(200).json(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+
+  const getUsersById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("select * from users where id = ?", [id])
+      .then(([users]) => {
+        if (users[0] != null) {
+          res.json(users[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+  
+  module.exports = {getUsers, getUsersById};
