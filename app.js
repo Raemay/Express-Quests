cc02979d0ff4ae27c317461b547363e4f37e8d60
Raemay <[melaniebelliard@outlook.fr](mailto:melaniebelliard@outlook.fr)>
2023-05-25 09:45:32 +0200

    quest 04 bis express

--- app.js
+++ app.js
@@ -19,16 +19,17 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 const userHandlers = require("./userHandlers");
+const { validateMovie, validateUser } = require("./validators");
 app.get("/api/users", userHandlers.getUsers)
 app.get("/api/users/:id", userHandlers.getUsersById);
 
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", validateUser, userHandlers.postUser);
 
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 
-app.put("/api/users/:id", userHandlers.updateUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
